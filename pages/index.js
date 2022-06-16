5fcfcad98b5b012d8e1fd67507f6eb1d2379a3d1
heinhwin <heinhwin23@gmail.com>
2022-06-15 23:36:39 -0700

    product heading change

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ const Home = ({ products, bannerData }) => (
       <HeroBanner heroBanner={bannerData.length && bannerData[0]}/>
 
       <div className="products-heading">
-        <h2>Best Selling Products</h2>
+        <h2>Men Hoodies</h2>
         <p>Everything you need for the GYM</p>
       </div>
 
